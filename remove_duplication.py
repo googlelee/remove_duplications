66c0ac0589b3a53a83f6cb1edb2a663aec4a4af9
DMGK <835306541@qq.com>
2020-09-03 21:37:12 +0800

    Add files via upload

---
+++ remove_duplication.py
@@ -0,0 +1,11 @@
+from imagededup.methods import PHash
+import os
+
+phasher = PHash()
+image_dir = r'Cropped/not_linmian'
+
+if __name__ == '__main__':
+    duplicates_list = phasher.find_duplicates_to_remove(image_dir)
+    for i in duplicates_list:
+        os.remove(os.path.join(image_dir, i))
+    print(len(duplicates_list))
